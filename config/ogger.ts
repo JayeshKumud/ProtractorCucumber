4cb205c442882a4b2e4a208eeae0118d12e03a0a
KUMUD <jayesh.kumud@sogeti.com>
2020-04-06 18:23:10 +0200

    oller

---
+++ config/ogger.ts
@@ -0,0 +1,37 @@
+export class Logger {
+    static Log(message: any, level: Level = Level.debug): void {
+
+        var log4js = require('log4js');
+        log4js.configure('./config/log4js.json');
+        var log = log4js.getLogger(); //for both console and file
+
+        if (level == Level.debug) {
+            log.level = 'debug';
+            log.debug(message);
+        } else if (level == Level.trace) {
+            log.level = 'trace';
+            log.trace(message);
+        } else if (level == Level.warn) {
+            log.level = 'debug';
+            log.debug(message);
+        } else if (level == Level.info) {
+            log.level = 'info';
+            log.info(message);
+        } else if (level == Level.error) {
+            log.level = 'error';
+            log.error(message);
+        } else if (level == Level.fatal) {
+            log.level = 'fatal';
+            log.fatal(message);
+        }
+    }
+}
+
+export enum Level {
+    debug,
+    trace,
+    info,
+    warn,
+    error,
+    fatal
+}
